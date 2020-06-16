d6fa4f995ac9984458238363ac1f19a562478037
tisanti147 <santid147@gmail.com>
2020-06-15 21:02:50 -0300

    cositas de la clase vuelo :)

--- src/Usuario/Vuelo.java
+++ src/Usuario/Vuelo.java
@@ -3,22 +3,47 @@ package Usuario;
 import com.company.Avion;
 import com.company.Bronce;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vuelo {
 
     private String fecha;
-    private List<Avion> list_avion;
+    private Avion avion;
     private int cantPasajeros;
     private double costoTotal;
     //HashSet <Provincia> origen;
     //HashSet <Provincia> destino;
 
+    public Vuelo (String fecha, int cantPasajeros, Avion avion, double costoTotal){
+        this.fecha = fecha;
+        this.cantPasajeros = cantPasajeros;
+        this.avion = avion;
+        this.costoTotal = costoTotal;
+    }
+
+    @Override
+    public String toString(){
+        return "Fecha del vuelo: " + getFecha() + "\nPasajeros que abordan: " + getCantPasajeros() + "\n\nINFORMACIÓN DEL AVIÓN:\n" + avion.toString() + "\n\nCosto total: " + getCostoTotal();
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public int getCantPasajeros() {
+        return cantPasajeros;
+    }
+
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+
     //(Cantidad de kms * Costo del km) + (cantidad de pasajeros * 3500) + (Tarifa del tipo de avión)
 
-    /*public int calcularCosto (){
-
-    }*/
+    public int calcularCosto (Avion avion, int pasajeros, int distancia){
+        return (distancia * avion.getCostoPorKM()) + (pasajeros * 3500) + avion.getTarifaAvion();
+    }
 
     // cancelarVuelo
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import Usuario.Vuelo;
+
 public class Main {
 
     public static void main(String[] args) {
-        Bronce avion = new Bronce(3000, 200, 50, 700, TipoPropulsion.HELICE);
-        System.out.println(avion.toString());
+        Bronce avion = new Bronce(3000, 150, 50, 700, TipoPropulsion.HELICE);
+
+        //(Cantidad de kms * Costo del km) + (cantidad de pasajeros * 3500) + (Tarifa del tipo de avión)
+
+        Vuelo vuelo1 = new Vuelo("2 de mayo", 5, avion, 0);
+
+        Vuelo vuelo2 = new Vuelo("2 de mayo", 5, avion, vuelo1.calcularCosto(avion, 5, 1100));
+
+        System.out.println(vuelo2.toString());
     }
 
 
