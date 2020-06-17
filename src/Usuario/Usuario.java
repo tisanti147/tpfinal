64f89698e89d1e1c1d5261b8f254412200ac427c
lucia <luli.icasatti@gmail.com>
2020-06-17 18:07:17 -0300

    modificaciones usuario

--- src/Usuario/Usuario.java
+++ src/Usuario/Usuario.java
@@ -8,9 +8,33 @@ public class Usuario {
     private int dni;
     private int edad;
     private Cliente infoVuelo;
+    private static int contadorUsuario=0;
 
     public Usuario() {
     }
+
+    public Usuario(Cliente client)
+    {
+        this.contadorUsuario++;
+        this.id=contadorUsuario;
+        this.nombre="";
+        this.apellido="";
+        this.dni=0;
+        this.edad=0;
+        this.infoVuelo=client;
+    }
+
+    public Usuario(int dni)
+    {
+        this.contadorUsuario++;
+        this.id=contadorUsuario;
+        this.nombre="";
+        this.apellido="";
+        this.dni=dni;
+        this.edad=0;
+        this.infoVuelo=null;
+    }
+
 
     public Usuario(int id, String nombre, String apellido, int dni, int edad, Cliente infoVuelo) {
         this.id = id;
@@ -20,5 +44,4 @@ public class Usuario {
         this.edad = edad;
         this.infoVuelo = infoVuelo;
     }
-
 }
